e469b72f1b6ec5d43cb85c0df69c521951031dc0
hangfengnice <nicehangfeng@foxmail.com>
2019-07-21 18:07:31 +0800

    book-detail-input
    短评的实现
    wxml 里写 wxs

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -2,10 +2,11 @@
 import {
   BookModel
 } from '../../models/book'
+import { LikeModel } from '../../models/like';
 const bookModel = new BookModel()
+const likeModel = new LikeModel()
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -13,9 +14,9 @@ Page({
     comments: [],
     book: null,
     likeStatus: false,
-    likeCount: 0
+    likeCount: 0,
+    posting: false
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -32,7 +33,7 @@ Page({
       })
     })
     comments.then(res => {
-      console.log(res)
+      // console.log(res)
       this.setData({
         comments: res.comments
       })
@@ -42,6 +43,55 @@ Page({
       this.setData({
         likeStatus: res.like_status,
         likeCount: res.fav_nums
+      })
+    })
+  },
+  
+  onLike(event){
+    const like_or_cancel = event.detail.behavior
+    likeModel.like(like_or_cancel, this.data.book.id, 400)
+  },
+
+  onFakePost(){
+    this.setData({
+      posting: true
+    })
+  },
+
+  onCancel(){
+    this.setData({
+      posting: false
+    })
+  },
+
+  onPost(event){
+    const comment = event.detail.text || event.detail.value
+    if(!comment){
+      return
+    }
+    if(comment.length > 12){
+      wx.showToast({
+        title: "短评最多12个字",
+        icon: 'none'
+      })
+      return
+    }
+    console.log(this.data.book.id)
+    bookModel.postComment(this.data.book.id, comment)
+    .then(res => {
+      wx.showToast({
+        title: "+1",
+        icon: 'none'
+      })
+      console.log(res)
+      this.data.comments.unshift({
+        content: comment,
+        nums: 1
+      })
+
+      this.setData({
+        comments: this.data.comments,
+        posting: false
       })
     })
   },
