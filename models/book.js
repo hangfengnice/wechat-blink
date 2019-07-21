4b485a1001d22edd207c2b0db42adf31e6d6f0dc
hangfengnice <nicehangfeng@foxmail.com>
2019-07-21 15:19:02 +0800

    封装文本 组件
    通过自定义样式 修改组件样式 (两种方式)
    小程序插槽的使用

--- models/book.js
+++ models/book.js
@@ -12,6 +12,24 @@ class BookModel extends HTTP{
       url: "book/favor/count",
     })
   }
+  // book 细节
+  getDetail(bid){
+    return this.request({
+      url: `book/${bid}/detail`
+    })
+  }
+  // book 喜欢
+  getLikeStatus(bid){
+    return this.request({
+      url: `book/${bid}/favor`
+    })
+  }
+  // book pingl
+  getComments(bid){
+    return this.request({
+      url: `book/${bid}/short_comment`
+    })
+  }
 }
 
 export {BookModel}
